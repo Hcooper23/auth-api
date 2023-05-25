33e47cba3c39fe479a6be3a5004956eaf6ca3a62
Hayden James Cooper <haydencpr95@gmail.com>
2023-05-24 21:32:20 -0400

    File structure update

--- src/server.js
+++ src/server.js
@@ -6,9 +6,12 @@ const cors = require('cors');
 
 
 // Esoteric Resources
+const notFoundHandler = require('./error-handlers/404.js');
 const errorHandler = require('./error-handlers/500.js');
-const notFound = require('./error-handlers/404.js');
 const authRoutes = require('./auth/routes.js');
+const logger = require('./middleware/logger.js');
+const v1Routes = require('./routes/v1.js');
+
 
 // Prepare the express app
 const app = express();
@@ -19,15 +22,25 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(logger);
+
 // Routes
 app.use(authRoutes);
+app.use('/api/v1', v1Routes);
+
+
+// proof of life
+app.get('/', (req, res, next) => {
+  res.status(200).send('Server is alive!!!');
+});
+
 
 // Catchalls
-app.use(notFound);
+app.use('*', notFoundHandler);
 app.use(errorHandler);
 
 module.exports = {
-  server: app,
+  app,
   start: (port) => {
     app.listen(port, () => {
       console.log(`Server Up on ${port}`);
